b121fe93132a0efeec244af2ff7d0178cdcca8e0
zwild <zwildfree@gmail.com>
2018-12-29 15:34:46 +0800

    add Commentary

--- scalariform.el
+++ scalariform.el
@@ -26,6 +26,14 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+
+;;; Commentary:
+;; M-x: with command scalariform-format-region or scalariform-format-region-or-buffer.
+;; If you want to use a property file for scalariform, you can
+;; (setq scalariform-use-properties-file-p t)
+;; (setq scalariform-properties-file "~/.scalariform.conf")
+;; You can also customize the variable `scalariform-program-args' to past extra arguments to scalariform.
+
 ;;; Code:
 (defgroup scalariform nil
   "Group for scalariform.")
